6e6d4b31297b019015b54ec1917afa554f898de2
Clauderic Demers <me@ced.io>
2021-01-01 17:38:59 -0500

    Lazy memoization of useScrollableAncestors

--- packages/core/src/hooks/utilities/useScrollableAncestors.ts
+++ packages/core/src/hooks/utilities/useScrollableAncestors.ts
@@ -1,7 +1,36 @@
-import {useMemo} from 'react';
+import {useEffect, useRef} from 'react';
+import {useLazyMemo} from '@dnd-kit/utilities';
 
 import {getScrollableAncestors} from '../../utilities';
 
+const defaultValue: Element[] = [];
+
 export function useScrollableAncestors(node: HTMLElement | null) {
-  return useMemo(() => getScrollableAncestors(node), [node]);
+  const previousNode = useRef(node);
+
+  const ancestors = useLazyMemo<Element[]>(
+    (previousValue) => {
+      if (!node) {
+        return defaultValue;
+      }
+
+      if (
+        previousValue &&
+        node &&
+        previousNode.current &&
+        node.parentNode === previousNode.current.parentNode
+      ) {
+        return previousValue;
+      }
+
+      return getScrollableAncestors(node);
+    },
+    [node]
+  );
+
+  useEffect(() => {
+    previousNode.current = node;
+  }, [node]);
+
+  return ancestors;
 }
