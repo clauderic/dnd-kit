bf04b2b7736dad0be66a2c9136bf18ec0417df62
Clauderic Demers <me@ced.io>
2021-01-02 00:03:53 -0500

    Allow nullish values to be passed to useSensors

---
+++ .changeset/usesensors-nullish-values.md
@@ -0,0 +1,7 @@
+---
+'@dnd-kit/core': patch
+'@dnd-kit/modifiers': patch
+'@dnd-kit/sortable': patch
+---
+
+Allow nullish values to be passed to `useSensors` for conditional sensors.
--- packages/core/src/sensors/useSensors.ts
+++ packages/core/src/sensors/useSensors.ts
@@ -2,10 +2,13 @@ import {useMemo} from 'react';
 import {SensorDescriptor, SensorOptions} from './types';
 
 export function useSensors(
-  ...sensors: SensorDescriptor<any>[]
+  ...sensors: (SensorDescriptor<any> | undefined | null)[]
 ): SensorDescriptor<SensorOptions>[] {
   return useMemo(
-    () => [...sensors],
+    () =>
+      [...sensors].filter(
+        (sensor): sensor is SensorDescriptor<any> => sensor != null
+      ),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [...sensors]
   );
