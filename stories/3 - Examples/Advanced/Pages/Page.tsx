9ab8e4ae78ba84e2af01b8c07fc20a8252987208
Clauderic Demers <me@ced.io>
2021-03-22 10:30:41 -0400

    Update styles

--- stories/3 - Examples/Advanced/Pages/Page.tsx
+++ stories/3 - Examples/Advanced/Pages/Page.tsx
@@ -29,44 +29,38 @@ export const Page = forwardRef<HTMLDivElement, Props>(function Page(
   ref
 ) {
   return (
-    <div className={styles.Wrapper}>
-      <div
-        ref={ref}
-        className={classNames(
-          styles.Page,
-          active && styles.active,
-          clone && styles.clone,
-          insertPosition === Position.Before && styles.insertBefore,
-          insertPosition === Position.After && styles.insertAfter,
-          layout === Layout.Vertical && styles.vertical
-        )}
-        data-id={id}
-        {...props}
-      >
-        {index != null ? (
-          <span className={styles.PageNumber}>{index}</span>
-        ) : null}
-        {onRemove ? (
-          <span className={styles.Remove} onClick={onRemove}>
-            {trashIcon}
-          </span>
-        ) : null}
-      </div>
+    <div
+      ref={ref}
+      className={classNames(
+        styles.Page,
+        active && styles.active,
+        clone && styles.clone,
+        insertPosition === Position.Before && styles.insertBefore,
+        insertPosition === Position.After && styles.insertAfter,
+        layout === Layout.Vertical && styles.vertical
+      )}
+      data-id={id}
+      {...props}
+    >
+      {index != null ? (
+        <span className={styles.PageNumber}>{index}</span>
+      ) : null}
+      {onRemove ? (
+        <button className={styles.Remove} onClick={onRemove}>
+          {trashIcon}
+        </button>
+      ) : null}
     </div>
   );
 });
 
 const trashIcon = (
   <svg
-    width="18"
-    viewBox="0 0 28 32"
-    fill="none"
+    width="10"
+    height="10"
+    viewBox="0 0 22 22"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      d="M8 1.99791V4.00209C8 3.99991 8.00009 4 7.99406 4H20.0059C20.0009 4 20 4.00093 20 4.00209V1.99791C20 2.00009 19.9999 2 20.0059 2H7.99406C7.99908 2 8 1.99907 8 1.99791ZM1.99826 6H0V4H6V1.99791C6 0.894496 6.89451 0 7.99406 0H20.0059C21.1072 0 22 0.89826 22 1.99791V4H28V6H26.0017L25.9983 6.08326L25.04 29.0826C24.9724 30.703 23.6307 32 22.0066 32H5.9934C4.36907 32 3.02758 30.7036 2.96004 29.0826L2.00173 6.08326L1.99826 6ZM4 6L4.9583 28.9993C4.98136 29.5525 5.44476 30 5.9934 30H22.0066C22.5551 30 23.0187 29.552 23.0417 28.9993L24 6H4ZM17.0072 10V26H19.0072V10H17.0072ZM13 10V26H15V10H13ZM8.9928 10.0159V26.0159H10.9928V10.0159H8.9928Z"
-    />
+    <path d="M2.99998 -0.000206962C2.7441 -0.000206962 2.48794 0.0972617 2.29294 0.292762L0.292945 2.29276C-0.0980552 2.68376 -0.0980552 3.31682 0.292945 3.70682L7.58591 10.9998L0.292945 18.2928C-0.0980552 18.6838 -0.0980552 19.3168 0.292945 19.7068L2.29294 21.7068C2.68394 22.0978 3.31701 22.0978 3.70701 21.7068L11 14.4139L18.2929 21.7068C18.6829 22.0978 19.317 22.0978 19.707 21.7068L21.707 19.7068C22.098 19.3158 22.098 18.6828 21.707 18.2928L14.414 10.9998L21.707 3.70682C22.098 3.31682 22.098 2.68276 21.707 2.29276L19.707 0.292762C19.316 -0.0982383 18.6829 -0.0982383 18.2929 0.292762L11 7.58573L3.70701 0.292762C3.51151 0.0972617 3.25585 -0.000206962 2.99998 -0.000206962Z" />
   </svg>
 );
