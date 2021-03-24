536428de61909b5da9efc44c90a482310e66dd9e
Clauderic Demers <me@ced.io>
2021-03-24 10:45:11 -0400

    Update implementation of FirstArgument

--- packages/utilities/src/types.ts
+++ packages/utilities/src/types.ts
@@ -1,10 +1,10 @@
 export type Arguments<T> = T extends (...args: infer U) => any ? U : never;
 
-export type ArgumentAtIndex<
-  Func,
-  Index extends keyof Arguments<Func>
-> = Arguments<Func>[Index];
-
-export type FirstArgument<T> = ArgumentAtIndex<T, 0>;
+export type FirstArgument<T> = T extends (
+  firstArg: infer U,
+  ...args: Array<any>
+) => any
+  ? U
+  : never;
 
 export type Without<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
