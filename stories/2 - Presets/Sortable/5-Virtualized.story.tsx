c867e4fa0bb5f3fa29d43c834036c945697faac2
Bnaya Peretz <me@bnaya.net>
2021-04-08 16:01:50 +0300

    Multi list virtualization

--- stories/2 - Presets/Sortable/5-Virtualized.story.tsx
+++ stories/2 - Presets/Sortable/5-Virtualized.story.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
-import {createPortal} from 'react-dom';
-import VirtualList from 'react-tiny-virtual-list';
+import React, { useEffect, useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
+// import VirtualList from 'react-tiny-virtual-list';
+import VirtualList from '../../utilities/virtual/main';
 
 import {
   closestCenter,
@@ -20,25 +21,38 @@ import {
 
 import styles from './Virtualized.module.css';
 
-import {createRange} from '../../utilities';
-import {SortableItem, Props} from './Sortable';
-import {Item, Wrapper} from '../../components';
+import { createRange } from '../../utilities';
+import { SortableItem, Props } from './Sortable';
+import { Item, Wrapper } from '../../components';
 
 export default {
   title: 'Presets/Sortable/Virtualized',
 };
 
+const ITEM_SIZE = 64;
+
 function Sortable({
   adjustScale = false,
   strategy = verticalListSortingStrategy,
-  itemCount = 100,
+  itemCount = 300,
   handle = false,
   getItemStyles = () => ({}),
   modifiers,
 }: Props) {
+  const [scrollPosition, setScrollPosition] = useState(0);
   const [items, setItems] = useState(() =>
     createRange<string>(itemCount, (index) => `${index + 1}`)
   );
+
+  const itemsSplitPoint = Math.floor(itemCount / 2);
+
+  const itemsGroups = useMemo(() => {
+    return [
+      items.slice(0, itemsSplitPoint),
+      items.slice(itemsSplitPoint),
+    ] as const;
+  }, [items]);
+
   const [activeId, setActiveId] = useState<string | null>(null);
   const sensors = useSensors(
     useSensor(PointerSensor),
@@ -49,14 +63,29 @@ function Sortable({
   const getIndex: (id: string) => number = items.indexOf.bind(items);
   const activeIndex = activeId ? getIndex(activeId) : -1;
 
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    window.addEventListener('scroll', () => {
+      setScrollPosition(window.scrollY);
+    }, {
+      // @ts-ignore types aren't aligned yet with this spec addition
+      signal: abortController.signal
+    });
+
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, []);
+
   return (
     <DndContext
       sensors={sensors}
       collisionDetection={closestCenter}
-      onDragStart={({active}) => {
+      onDragStart={({ active }) => {
         setActiveId(active.id);
       }}
-      onDragEnd={({over}) => {
+      onDragEnd={({ over }) => {
         if (over) {
           const overIndex = getIndex(over.id);
           if (activeIndex !== overIndex) {
@@ -69,16 +98,51 @@ function Sortable({
       onDragCancel={() => setActiveId(null)}
       modifiers={modifiers}
     >
-      <Wrapper center>
+      <Wrapper center style={{
+        display: "block"
+      }}>
         <SortableContext items={items} strategy={strategy}>
           <VirtualList
+            scrollOffset={scrollPosition}
             width={500}
             height={600}
+            viewportHeight={window.document.documentElement.clientHeight}
             className={styles.VirtualList}
-            itemCount={items.length}
-            itemSize={64}
-            renderItem={({index, style}) => {
-              const id = items[index];
+            itemCount={itemsGroups[0].length}
+            itemSize={ITEM_SIZE}
+            renderItem={({ index, style }) => {
+              const id = itemsGroups[0][index];
+
+              return (
+                <SortableItem
+                  key={id}
+                  id={id}
+                  index={index}
+                  handle={handle}
+                  wrapperStyle={() => ({
+                    ...style,
+                    opacity: id === activeId ? 0 : undefined,
+                    padding: 5,
+                  })}
+                  style={getItemStyles}
+                />
+              );
+            }}
+          />
+          <div style={{
+            height: 200,
+            background: "red"
+          }}>SECOND LIST TITLE</div>
+          <VirtualList
+            scrollOffset={scrollPosition - 200 - (itemsGroups[0].length * ITEM_SIZE)}
+            width={500}
+            height={600}
+            viewportHeight={window.document.documentElement.clientHeight}
+            className={styles.VirtualList}
+            itemCount={itemsGroups[1].length}
+            itemSize={ITEM_SIZE}
+            renderItem={({ index, style }) => {
+              const id = itemsGroups[1][index];
 
               return (
                 <SortableItem
