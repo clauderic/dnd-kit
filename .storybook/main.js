cac10970aa0c47cc3d3f90be75edba8a8ca692ed
Roman Akhmadullin <akhmadullin_ri@drom.ru>
2021-05-28 22:49:29 +1000

    Add @storybook/addon-storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/addon-storysource',
   ],
   webpackFinal: async (config) => {
     config.module.rules = [
