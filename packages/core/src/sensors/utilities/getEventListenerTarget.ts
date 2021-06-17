495f236fe25dc100fbf7682ff6610e4246d9e9f0
Clauderic Demers <me@ced.io>
2021-06-17 12:15:55 -0400

    Fix `getEventListenerTarget` when target element is not an HTMLElement
    For example, when the target element is an SVGElement, the event listener target returned was the owner document instead.
    
    This causes issues with touch events when the `target` is an SVGElement. If the `event.target` element is removed from the document events will still be targeted at it, and hence won't always bubble up to the window or document anymore. If there is any risk of an element being removed while it is being dragged, the best practice is to attach the event listeners directly to the target.

--- packages/core/src/sensors/utilities/getEventListenerTarget.ts
+++ packages/core/src/sensors/utilities/getEventListenerTarget.ts
@@ -2,6 +2,11 @@ import {getOwnerDocument} from '../../utilities';
 
 export function getEventListenerTarget(
   element: EventTarget | null
-): HTMLElement | Document {
-  return element instanceof HTMLElement ? element : getOwnerDocument(element);
+): EventTarget | Document {
+  // If the `event.target` element is removed from the document events will still be targeted
+  // at it, and hence won't always bubble up to the window or document anymore.
+  // If there is any risk of an element being removed while it is being dragged,
+  // the best practice is to attach the event listeners directly to the target.
+  // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
+  return element instanceof EventTarget ? element : getOwnerDocument(element);
 }
