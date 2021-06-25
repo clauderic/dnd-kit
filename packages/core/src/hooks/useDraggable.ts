f5097fe7fb509b38205fec0c1803e0dec0ccb8e8
Billy Main <wmain@netflix.com>
2021-06-25 13:26:40 -0700

    refactor: update CollisionDetection to accept DraggingNode and DroppableContainer[]

--- packages/core/src/hooks/useDraggable.ts
+++ packages/core/src/hooks/useDraggable.ts
@@ -54,7 +54,7 @@ export function useDraggable({
 
   useEffect(
     () => {
-      draggableNodes[id] = {node, data: dataRef};
+      draggableNodes[id] = {id, node, data: dataRef};
 
       return () => {
         delete draggableNodes[id];
