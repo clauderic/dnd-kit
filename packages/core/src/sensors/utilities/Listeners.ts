99e35b5dc7c251d3ca8d95be5cdbc133ce871a82
Clauderic Demers <me@ced.io>
2021-07-22 13:40:57 -0400

    Stop propagation of click events on draggable node once activation constraints are met

--- packages/core/src/sensors/utilities/Listeners.ts
+++ packages/core/src/sensors/utilities/Listeners.ts
@@ -1,23 +1,24 @@
 export class Listeners {
-  private listeners: {
-    eventName: string;
-    handler: EventListenerOrEventListenerObject;
-  }[] = [];
+  private listeners: [
+    string,
+    EventListenerOrEventListenerObject,
+    AddEventListenerOptions | boolean | undefined
+  ][] = [];
 
-  constructor(private target: EventTarget) {}
+  constructor(private target: EventTarget | null) {}
 
-  public add(
+  public add<T extends Event>(
     eventName: string,
-    handler: EventListenerOrEventListenerObject,
-    options?: AddEventListenerOptions | false
+    handler: (event: T) => void,
+    options?: AddEventListenerOptions | boolean
   ) {
-    this.target.addEventListener(eventName, handler, options);
-    this.listeners.push({eventName, handler});
+    this.target?.addEventListener(eventName, handler as EventListener, options);
+    this.listeners.push([eventName, handler as EventListener, options]);
   }
 
-  public removeAll() {
-    this.listeners.forEach(({eventName, handler}) =>
-      this.target.removeEventListener(eventName, handler)
+  public removeAll = () => {
+    this.listeners.forEach((listener) =>
+      this.target?.removeEventListener(...listener)
     );
-  }
+  };
 }
