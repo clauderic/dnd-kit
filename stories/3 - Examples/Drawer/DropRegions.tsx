a53be675fdd3b3f209fb3876c98e52ed2287c4a3
Claudéric Demers <clauderic.demers@shopify.com>
2022-03-11 10:27:25 -0500

    Conditionally mount/unmount drop regions

--- stories/3 - Examples/Drawer/DropRegions.tsx
+++ stories/3 - Examples/Drawer/DropRegions.tsx
@@ -5,12 +5,16 @@ import {Region} from './constants';
 import styles from './Drawer.module.css';
 
 export function DropRegions() {
-  const {setNodeRef: setExpandRegionNodeRef} = useDroppable({
+  const {active, setNodeRef: setExpandRegionNodeRef} = useDroppable({
     id: Region.Expand,
   });
   const {setNodeRef: setCollapseRegionRef} = useDroppable({
     id: Region.Collapse,
   });
+
+  if (!active) {
+    return null;
+  }
 
   return (
     <div className={styles.DropRegions}>
