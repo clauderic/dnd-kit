268f12c3f1cf10c9d92d43ab313079df06f067c6
Jaril <jarilvalenciano@gmail.com>
2022-08-18 18:55:50 -0700

    Add replay

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -12,10 +12,14 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const cypressReplay = require("@replayio/cypress");
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+  cypressReplay.default(on, config);
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  return config;
 }
---
+++ replay_ts.md
@@ -0,0 +1,2 @@
+[Team invitation](https://app.replay.io/team/invitation?code=1fe2c677-fd2f-4c51-8f45-7f61d79fa209)
+[Link to team](https://app.replay.io/team/dzpjMWFmNTU5NC01MTNkLTRkODAtOWU1YS1iZjI5YzA3OGFmYzI=/runs)
