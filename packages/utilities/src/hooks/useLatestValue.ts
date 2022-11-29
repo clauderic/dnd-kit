da7c60dcbb76d89cf1fcb421e69a4abcea2eeebe
Benjamin Koltes <ayc0.benj@gmail.com>
2022-11-28 21:50:29 -0500

    Upgrade TypeScript to 4.8 (#948)

--- packages/utilities/src/hooks/useLatestValue.ts
+++ packages/utilities/src/hooks/useLatestValue.ts
@@ -1,10 +1,11 @@
 import {useRef} from 'react';
+import type {DependencyList} from 'react';
 
 import {useIsomorphicLayoutEffect} from './useIsomorphicLayoutEffect';
 
 export function useLatestValue<T extends any>(
   value: T,
-  dependencies = [value]
+  dependencies: DependencyList = [value]
 ) {
   const valueRef = useRef<T>(value);
 
