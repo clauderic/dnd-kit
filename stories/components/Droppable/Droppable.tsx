10ec00dc109cbbeeed3f16d3a1cc539965b47354
Alissa <alissa.vrk@gmail.com>
2023-03-30 14:11:40 +0300

    remove dragging css class from droppable story, it did nothing and created and extra render

--- stories/components/Droppable/Droppable.tsx
+++ stories/components/Droppable/Droppable.tsx
@@ -7,11 +7,10 @@ import styles from './Droppable.module.css';
 
 interface Props {
   children: React.ReactNode;
-  dragging: boolean;
   id: UniqueIdentifier;
 }
 
-export function Droppable({children, id, dragging}: Props) {
+export function Droppable({children, id}: Props) {
   const {isOver, setNodeRef} = useDroppable({
     id,
   });
@@ -22,7 +21,6 @@ export function Droppable({children, id, dragging}: Props) {
       className={classNames(
         styles.Droppable,
         isOver && styles.over,
-        dragging && styles.dragging,
         children && styles.dropped
       )}
       aria-label="Droppable region"
