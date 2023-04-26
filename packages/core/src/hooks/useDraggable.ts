95583ab1a2150dcf472d448a53df2a865da57069
Sebastian Meier <s.meier@schuchert.de>
2023-04-26 17:06:14 +0200

    optimize internal context selector

--- packages/core/src/hooks/useDraggable.ts
+++ packages/core/src/hooks/useDraggable.ts
@@ -1,19 +1,20 @@
-import {useCallback, useMemo} from 'react';
+import { useCallback, useMemo } from 'react';
 import {
   useIsomorphicLayoutEffect,
   useLatestValue,
   useUniqueId,
   usePassiveNodeRef,
 } from '@schuchertmanagementberatung/dnd-kit-utilities';
-import type {Data} from '../store';
-import type {UniqueIdentifier} from '../types';
-import {useSyntheticListeners, SyntheticListenerMap} from './utilities';
+import type { Data } from '../store';
+import type { UniqueIdentifier } from '../types';
+import { useSyntheticListeners, SyntheticListenerMap } from './utilities';
 import {
   ActiveDraggableContextStore,
+  InternalContextStore,
   useActiveDraggableContextStore,
   useInternalContextStore,
 } from '../store/new-store';
-import {shallow} from 'zustand/shallow';
+import { shallow } from 'zustand/shallow';
 
 export interface UseDraggableArguments {
   id: UniqueIdentifier;
@@ -48,30 +49,32 @@ export function useDraggable({
   attributes,
 }: UseDraggableArguments) {
   const key = useUniqueId(ID_PREFIX);
+  const internalContextSelector = useCallback(
+    (state: InternalContextStore) => {
+      return {
+        activators: state.activators,
+        activatorEvent: state.activatorEvent,
+        activeNodeRect: state.activeNodeRect,
+        ariaDescribedByIdDraggable: state.ariaDescribedById?.draggable,
+        draggableNodes: state.draggableNodes,
+        isDragging: state.active?.id === id,
+      };
+    },
+    [id]
+  );
   const {
     activators,
     activatorEvent,
     activeNodeRect,
-    ariaDescribedById,
+    ariaDescribedByIdDraggable,
     draggableNodes,
-    activeId,
-  } = useInternalContextStore(
-    (state) => ({
-      activators: state.activators,
-      activatorEvent: state.activatorEvent,
-      activeNodeRect: state.activeNodeRect,
-      ariaDescribedById: state.ariaDescribedById,
-      draggableNodes: state.draggableNodes,
-      activeId: state.active?.id,
-    }),
-    shallow
-  );
+    isDragging,
+  } = useInternalContextStore(internalContextSelector, shallow);
   const {
     role = defaultRole,
     roleDescription = 'draggable',
     tabIndex = 0,
   } = attributes ?? {};
-  const isDragging = activeId === id;
   const activeDraggableSelector = useCallback(
     (state: ActiveDraggableContextStore) => {
       if (isDragging) {
@@ -92,7 +95,7 @@ export function useDraggable({
 
   useIsomorphicLayoutEffect(
     () => {
-      draggableNodes.set(id, {id, key, node, activatorNode, data: dataRef});
+      draggableNodes.set(id, { id, key, node, activatorNode, data: dataRef });
 
       return () => {
         const node = draggableNodes.get(id);
@@ -113,7 +116,7 @@ export function useDraggable({
       'aria-disabled': disabled,
       'aria-pressed': isDragging && role === defaultRole ? true : undefined,
       'aria-roledescription': roleDescription,
-      'aria-describedby': ariaDescribedById.draggable,
+      'aria-describedby': ariaDescribedByIdDraggable,
     }),
     [
       disabled,
@@ -121,7 +124,7 @@ export function useDraggable({
       tabIndex,
       isDragging,
       roleDescription,
-      ariaDescribedById.draggable,
+      ariaDescribedByIdDraggable,
     ]
   );
 
