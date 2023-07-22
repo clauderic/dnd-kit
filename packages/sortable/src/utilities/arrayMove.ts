4cd4d309b8e66e8c084c8aa8d5a9ed5cf9e44644
Natan Fernandes <natanfernandes@protonmail.com>
2023-07-22 06:09:47 -0300

    Fix weird issue in arrayMove

--- packages/sortable/src/utilities/arrayMove.ts
+++ packages/sortable/src/utilities/arrayMove.ts
@@ -3,11 +3,6 @@
  */
 export function arrayMove<T>(array: T[], from: number, to: number): T[] {
   const newArray = array.slice();
-  newArray.splice(
-    to < 0 ? newArray.length + to : to,
-    0,
-    newArray.splice(from, 1)[0]
-  );
-
+  [newArray[from], newArray[to]] = [newArray[from], newArray[to]];
   return newArray;
 }
