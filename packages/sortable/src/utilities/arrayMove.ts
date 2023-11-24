de5a3763d5a1f9dac15ce724c8f6436b80e73518
ValentinIlnitskiy <79172266+ValentinIlnitskiy@users.noreply.github.com>
2023-11-24 21:09:09 +0100

    Remove unnecessary expression from arrayMove function

--- packages/sortable/src/utilities/arrayMove.ts
+++ packages/sortable/src/utilities/arrayMove.ts
@@ -3,11 +3,7 @@
  */
 export function arrayMove<T>(array: T[], from: number, to: number): T[] {
   const newArray = array.slice();
-  newArray.splice(
-    to < 0 ? newArray.length + to : to,
-    0,
-    newArray.splice(from, 1)[0]
-  );
+  newArray.splice(to, 0, newArray.splice(from, 1)[0]);
 
   return newArray;
 }
