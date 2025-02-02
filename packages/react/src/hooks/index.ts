6978d81311e5ac0d9c623b8ceb864740c693ca1d
Clauderic Demers <clauderic.d@gmail.com>
2025-02-02 11:03:59 -0500

    Introuce `useDeepSignal` hook

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -2,6 +2,7 @@
 
 export {useConstant} from './useConstant.ts';
 export {useComputed} from './useComputed.ts';
+export {useDeepSignal} from './useDeepSignal.ts';
 export {useImmediateEffect} from './useImmediateEffect.ts';
 export {useIsomorphicLayoutEffect} from './useIsomorphicLayoutEffect.ts';
 export {useLatest} from './useLatest.ts';
---
+++ packages/react/src/hooks/useDeepSignal.ts
@@ -0,0 +1,53 @@
+import {useEffect, useMemo, useRef, useState} from 'react';
+import {effect, untracked} from '@dnd-kit/state';
+
+import {useIsomorphicLayoutEffect} from './useIsomorphicLayoutEffect.ts';
+import {useForceUpdate} from './useForceUpdate.ts';
+
+/** Trigger a re-render when reading signal properties of an object. */
+export function useDeepSignal<T extends object | null | undefined>(
+  target: T
+): T {
+  const tracked = useRef(new Map<string | symbol, any>());
+  const forceUpdate = useForceUpdate();
+
+  useIsomorphicLayoutEffect(() => {
+    if (!target) {
+      tracked.current.clear();
+      return;
+    }
+
+    return effect(() => {
+      let stale = false;
+
+      for (const entry of tracked.current) {
+        const [key] = entry;
+        const value = untracked(() => entry[1]);
+        const latestValue = (target as any)[key];
+
+        if (value !== latestValue) {
+          stale = true;
+          tracked.current.set(key, latestValue);
+        }
+      }
+
+      if (stale) forceUpdate();
+    });
+  }, [target]);
+
+  return useMemo(
+    () =>
+      target
+        ? new Proxy(target, {
+            get(target, key) {
+              const value = (target as any)[key];
+
+              tracked.current.set(key, value);
+
+              return value;
+            },
+          })
+        : target,
+    [target]
+  );
+}
