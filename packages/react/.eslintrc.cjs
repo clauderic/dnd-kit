297d13d22b7da02cbd08016d0ccb873fe35d48ff
Hannes Küttner <kuettner.hannes@gmail.com>
2025-04-18 00:07:54 +0200

    chore: refactor eslint configs

--- packages/react/.eslintrc.cjs
+++ packages/react/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   extends: [
-    'prettier',
+    'react-app',
   ],
 };
--- packages/vue/.eslintrc.cjs
+++ packages/vue/.eslintrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   extends: [
+    'plugin:vue/recommended',
     'prettier',
   ],
 };
