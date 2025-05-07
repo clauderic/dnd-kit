13e9b9460fe767321ef548a65b2d411ca57263b2
Guillaume Salles <guillaume.salles@shopify.com>
2025-05-07 11:40:48 -0400

    Ensure showPopover is not called when popover is already visible

--- packages/dom/src/utilities/popover/showPopover.ts
+++ packages/dom/src/utilities/popover/showPopover.ts
@@ -4,7 +4,8 @@ export function showPopover(element: Element) {
   if (
     supportsPopover(element) &&
     element.isConnected &&
-    element.hasAttribute('popover')
+    element.hasAttribute('popover') &&
+    !element.matches(':popover-open')
   ) {
     element.showPopover();
   }
