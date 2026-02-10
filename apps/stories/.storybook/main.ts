c9c9cd6038ece759c2ef38a94b860bdd872c8463
Clauderic Demers <clauderic.d@gmail.com>
2026-02-09 19:28:19 -0500

    Add SolidJS storybook ref

--- apps/stories/.storybook/main.ts
+++ apps/stories/.storybook/main.ts
@@ -23,8 +23,13 @@ export default {
     if (process.env.VANILLA_STORYBOOK_URL) {
       refs.vanilla = {title: 'Vanilla', url: process.env.VANILLA_STORYBOOK_URL};
     }
+
     if (process.env.VUE_STORYBOOK_URL) {
       refs.vue = {title: 'Vue', url: process.env.VUE_STORYBOOK_URL};
+    }
+
+    if (process.env.SOLID_STORYBOOK_URL) {
+      refs.solid = {title: 'Solid', url: process.env.SOLID_STORYBOOK_URL};
     }
 
     return refs;
