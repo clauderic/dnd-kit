1b8713a3928f0b9bae4610771b7d10f94175c0fe
Arpita Singh <arpita.singh@iss-stoxx.com>
2026-08-06 13:11:44 +0530

    fix(dom): removed unwanted file.

--- packages/dom/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-// packages/dom/src/global.d.ts
-declare global {
-  // Optional registry of extra Document objects (e.g. window.open popups) for
-  // cross-window drag support. Host apps populate this via registerDndDocument().
-  // eslint-disable-next-line no-var
-  var __dndKitDocuments__: Set<Document> | undefined;
-}
-
-export {};
